afe24516bfa6472ce8ff16136713f823ea119cbf
hayase <89891988+s-hayase@users.noreply.github.com>
2023-01-16 21:07:01 +0900

    ランキングページを追加

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const Diary = require('../models/diary');
+const User = require('../models/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sanpo nikki', user: req.user });
+router.get('/', async function(req, res, next) {
+  const today = new Date().toLocaleDateString();
+  const diaries = await Diary.findAndCountAll({
+    include: [
+      {
+        model: User,
+        attributes: ['userId',`username`]
+      }],
+    where: {
+      date: today
+    },
+    order: [['step', 'DESC']],
+    limit:10
+  });
+  res.render('index', { title: 'sanpo nikki', user: req.user,diaries:diaries.rows,today:today});
 });
 
 module.exports = router;
